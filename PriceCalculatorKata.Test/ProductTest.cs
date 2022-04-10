9b8e28ea5526671fa1e47fd7d64d16c33e694bf2
sabahb3 <sabahbaara4@gmail.com>
2022-04-10 10:29:57 +0300

    Refactoring Test of Tax calculation

--- PriceCalculatorKata.Test/ProductTest.cs
+++ PriceCalculatorKata.Test/ProductTest.cs
@@ -8,43 +8,33 @@ public class ProductTest
 {
     private Mock<ITax> _tax;
     private Product _product;
+    private Mock<IDiscount> _UniversalDiscount;
 
     public ProductTest()
     {
         _tax = new Mock<ITax>();
          _product = new Product(12345,"The Little Prince",20.25,_tax.Object);
-
+         _UniversalDiscount = new Mock<IDiscount>();
     }
 
-    [Fact]
-    public void ShouldCalculateTaxAmountBasedOnProductPrice()
+    [Theory]
+    [InlineData(20,20.25,4.05,24.30)]
+    [InlineData(21,20.25,4.25,24.50)]
+
+    public void ShouldCalculateTaxAmountBasedOnProductPrice(int  tax, double price,double taxAmout,double finalPrice)
     {
         // Arrange
-        _tax.Setup(x => x.TaxValue).Returns(20);
+        _tax.Setup(x => x.TaxValue).Returns(tax);
 
         // Act
-        var taxAmount = _product.Tax;
-        var finalPrice = _product.FinalPrice;
+        var actualTaxAmount = _product.Tax;
+        var actualFinalPrice = _product.FinalPrice;
         
         // Assert
-        Assert.Equal(20.25,_product.Price);
-        Assert.Equal(4.05,taxAmount);
-        Assert.Equal(24.30,finalPrice);
+        Assert.Equal(price,_product.Price);
+        Assert.Equal(taxAmout,actualTaxAmount);
+        Assert.Equal(finalPrice,actualFinalPrice);
     }
     
-    [Fact]
-    public void ShouldCalculateTaxAmountBasedOnProductPriceWhenTaxIs21Percent()
-    {
-        // Arrange
-        _tax.Setup(x => x.TaxValue).Returns(21);
 
-        // Act
-        var taxAmount = _product.Tax;
-        var finalPrice = _product.FinalPrice;
-        
-        // Assert
-        Assert.Equal(20.25,_product.Price);
-        Assert.Equal(4.25,taxAmount);
-        Assert.Equal(24.50,finalPrice);
-    }
 }
