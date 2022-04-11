8a8ccbf1a32fe8958e8853baff4fa16a3ebfb476
sabahb3 <sabahbaara4@gmail.com>
2022-04-11 10:51:06 +0300

    Update the Discount property to check if the product has a UPC discount in order to take it into account while calculating the total discount and final price.

--- PriceCalculatorKata/Product.cs
+++ PriceCalculatorKata/Product.cs
@@ -7,14 +7,16 @@ public class Product: IProduct
 {
     private ITax _tax;
     private IDiscount _universalDiscount;
+    private ISpecialDiscount _upcDiscount;
     private double _price;
-    public Product(int upc, string name, double price, ITax tax,IDiscount universalDiscount)
+    public Product(int upc, string name, double price, ITax tax,IDiscount universalDiscount, ISpecialDiscount upcDiscount)
     {
         UPC = upc;
         Name = name ?? String.Empty;
         Price = price;
         _tax = tax;
         _universalDiscount = universalDiscount;
+        _upcDiscount = upcDiscount;
     }
 
     public double Price
@@ -50,8 +52,15 @@ public class Product: IProduct
     {
         get
         {
-            var discount = new FormattedDouble(_universalDiscount.DiscountValue / 100.0).FormattedNumber;
-            return new FormattedDouble(Price * discount).FormattedNumber;
+            var upcDiscount = 0d;
+            if (_upcDiscount.Contains(UPC,out var specialDiscount))
+            {
+                var upcDiscountRatio = new FormattedDouble(specialDiscount!.DiscountValue / 100.0).FormattedNumber;
+                upcDiscount = new FormattedDouble(Price * upcDiscountRatio).FormattedNumber;
+            }
+            var universalDiscountRatio = new FormattedDouble(_universalDiscount.DiscountValue / 100.0).FormattedNumber;
+            var universalDiscount= new FormattedDouble(Price * universalDiscountRatio).FormattedNumber;
+            return upcDiscount + universalDiscount;
         }
     }
 }
